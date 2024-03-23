3b27ec2bb2e8529103fc440d717936f73af2b90b
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2024-03-23 08:58:02 +0800

    Add Statuolumn Home Content in Extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,38 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class StatolumnViewProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'statolumn.home';
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		_context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [this._extensionUri],
+		};
+
+		webviewView.webview.html = `<html></html>`;
+
+		webviewView.webview.onDidReceiveMessage((data) => {
+			switch (data.type) {
+				case 'alert':
+					vscode.window.showErrorMessage(data.value);
+					return;
+			}
+		});
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,6 +52,10 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const statolumn_provider = new StatolumnViewProvider(context.extensionUri);
+
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider(StatolumnViewProvider.viewType, statolumn_provider));
 }
 
 // This method is called when your extension is deactivated
